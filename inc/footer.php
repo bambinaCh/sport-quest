0e76c1b771250ffa60d2f3cb2020fc35a28b7e7d
bambinaCh <chaimaa.jarite@gmail.com>
2022-03-24 16:10:04 +0100

    PAGES CH

--- inc/footer.php
+++ inc/footer.php
@@ -1,36 +1,7 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title>Footer</title>
-    <link rel='stylesheet' type='text/css' media='screen' href='css/main.css'>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-</head>
-
-<body>
-
-    <div class="footer">
-
-        <!--
-            <?php
-            $menuItems = array(
-                array("/homepage.php", "Home"),
-                array("/index.php", "Survey"),
-                array("/library.php", "Library")
-            );
-            foreach ($menuItems as $item) {
-                echo "<a href ='$item[0]'>$item[1]</a> ";
-            }
-            ?>
-      -->
+<div class="footer">
 
 
+    with ♥ by SportQuest Team © <?= date('Y') ?>
 
-        with ♥ by SportQuest Team © <?= date('Y') ?>
+</div>
 
-    </div>
-
-
-</body>
-
-</html>
--- inc/header.php
+++ inc/header.php
@@ -1,47 +1,28 @@
-<!DOCTYPE html>
-<html>
 
-<head>
-    <title>Header</title>
-    <link rel='stylesheet' type='text/css' media='screen' href='css/main.css'>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+<nav class="topnav" id="topnav">
+    <a href="homepage.php"><img class="img-responsive" src="/img/logo_w.svg" data-bs-hover-animate="pulse" alt="logo" width="48" height="48"></a>
+    <div class="navbar">
+        <strong class="surveyItem">
+            <?php
+            $menuItems = array(
+                array("/question1.php", "Quiz"),
 
-</head>
-
-<body>
-
-
-    <nav class="topnav" id="topnav">
-        <a href="homepage.php"><img class="img-responsive" src="/img/logo_w.svg" data-bs-hover-animate="pulse" alt="logo" width="48" height="48"></a>
-        <div class="navbar">
-            <strong class="surveyItem">
-                <?php
-                $menuItems = array(
-                    array("/index.php", "Survey"),
-
-                );
-                foreach ($menuItems as $item) {
-                    echo "<a href ='$item[0]'>$item[1]</a> ";
-                }
-                ?>
-            </strong>
-        </div>
-    </nav>
-
-
-    <script>
-        function myFunction() {
-            var x = document.getElementById("topnav");
-            if (x.className === "topnav") {
-                x.className += " responsive";
-            } else {
-                x.className = "topnav";
+            );
+            foreach ($menuItems as $item) {
+                echo "<a href ='$item[0]'>$item[1]</a> ";
             }
+            ?>
+        </strong>
+    </div>
+</nav>
+
+<script>
+    function myFunction() {
+        var x = document.getElementById("topnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
         }
-    </script>
-
-</body>
-
-</html>
+    }
+</script>
