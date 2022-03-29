9671cbb4636c61e1dbb55ea357387e961d33c2b3
bambinaCh <chaimaa.jarite@gmail.com>
2022-03-29 15:09:05 +0200

    adjusted footer and header

--- inc/header.php
+++ inc/header.php
@@ -1,20 +1,23 @@
 <!--  THIS IS (_Chaimaa_) PAGE -->
-<nav class="topnav" id="topnav">
-    <a href="homepage.php"><img class="img-responsive" src="/img/LOGO-FAVICON.png" data-bs-hover-animate="pulse" alt="logo" width="48" height="48"></a>
-    <div class="navbar">
-        <strong class="surveyItem">
-            <?php
-            $menuItems = array(
-                array("/question1.php", "Quiz"),
+<header>
+    <nav class="topnav" id="topnav">
+        <a href="homepage.php"><img class="img-responsive" src="/img/LOGO-FAVICON.png" data-bs-hover-animate="pulse" alt="logo" width="48" height="48"></a>
+        <div class="navbar">
+            <strong class="surveyItem">
+                <?php
+                $menuItems = array(
+                    array("/question1.php", "Quiz"),
 
-            );
-            foreach ($menuItems as $item) {
-                echo "<a href ='$item[0]'>$item[1]</a> ";
-            }
-            ?>
-        </strong>
-    </div>
-</nav>
+                );
+                foreach ($menuItems as $item) {
+                    echo "<a href ='$item[0]'>$item[1]</a> ";
+                }
+                ?>
+            </strong>
+        </div>
+    </nav>
+
+</header>
 
 <script>
     function myFunction() {
