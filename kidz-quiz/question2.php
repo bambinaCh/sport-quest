c724c14dea833e7f393094a157add071fb2efad4
amstutzsven <sven.amstutz@gmx.ch>
2022-03-30 15:27:30 +0200

    latest from the latest

--- kidz-quiz/question2.php
+++ kidz-quiz/question2.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$_SESSION["score"] = 0;
+$_SESSION["points-1"] = $_POST["points"];
 ?>
 
 <!--  THIS IS (_Sven_) PAGE -->
@@ -25,26 +25,23 @@ $_SESSION["score"] = 0;
 
         <form action="/kidz-quiz/question3.php" method="post" onsubmit="return validateRadio();">
 
-    <form id="butt1"><input type="radio" id= "butt1_Leather" value="Leather" checked>
-    <label for = "butt1_Leather" >Leather</label>
-    <form id="butt2"><input type="radio" id= "butt2_Gold" value="Gold" checked>
-    <label for = "butt2" >Gold</label>
-    <form id="butt3"><input type="radio" id= "butt3_Silver" value="Silver" checked>
-    <label for = "form3_no" >Silver</label>
-    <form id="butt4"><input type="radio" id= "butt4_Bronze" value="Bronze" checked>
-    <label for = "butt4" >Bronze</label>
+            <input type="radio" name="butt" id="butt1_Leather" value="Leather">
+            <label for="butt1_Leather" >Leather</label>
+            <input type="radio" name="butt" id="butt2_Gold" value="Gold">
+            <label for="butt2_Gold" >Gold</label>
+            <input type="radio" name="butt" id="butt3_Silver" value="Silver">
+            <label for="butt3_Silver" >Silver</label>
+            <input type="radio" name="butt" id="butt4_Bronze" value="Bronze">
+            <label for="butt4_Bronze" >Bronze</label>
 
-        <p id="validation-warning" class="warning"></p>
+            <p id="validation-warning" class="warning"></p>
 
-        <button type="submit" class="next-button">Next</button><div id="bugger" class="warning"></div>
-    </form>
-</main>
+            <button type="submit" class="next-button">Next</button><div id="bugger" class="warning"></div>
+        </form>
 
+    </main>
 
-<input type="hidden" name="lastPageID" value="question2">
-            <input type="hidden" name="points"  id="rings" value="">
-    <?php include '../inc/footer.php' ?>
-
+<?php include '../inc/footer.php' ?>
 
 </body>
 
